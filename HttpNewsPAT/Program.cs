7d83ad67740c674f9912e754756177af96eecb24
Kolovratik <Kolovratik@HOME-PC>
2024-12-22 12:50:44 +0300

    Получение информации с сайта

--- HttpNewsPAT/Program.cs
+++ HttpNewsPAT/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace HttpNewsPAT
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace HttpNewsPAT
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            WebRequest request = WebRequest.Create("http://127.0.0.1/login");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(response.StatusDescription);
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            Console.Read();
         }
     }
 }
