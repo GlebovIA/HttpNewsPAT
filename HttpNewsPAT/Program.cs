b01e60cd4bdc00a357811d68512516b1495d5a65
Kolovratik <Kolovratik@HOME-PC>
2024-12-22 14:04:27 +0300

    Получение печеньки авторизации

--- HttpNewsPAT/Program.cs
+++ HttpNewsPAT/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.IO;
+using System.Diagnostics;
 using System.Net;
+using System.Text;
 
 namespace HttpNewsPAT
 {
@@ -8,17 +9,29 @@ namespace HttpNewsPAT
     {
         static void Main(string[] args)
         {
-            WebRequest request = WebRequest.Create("http://127.0.0.1/login");
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Console.WriteLine(response.StatusDescription);
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
-            Console.WriteLine(responseFromServer);
-            reader.Close();
-            dataStream.Close();
-            response.Close();
+            SingIn("user", "user");
             Console.Read();
         }
+        public static void SingIn(string Login, string Password)
+        {
+            string url = "http://127.0.0.1/ajax/login.php";
+            Debug.WriteLine($"Выполняем запрос: {url}");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CookieContainer = new CookieContainer();
+            string postData = $"login={Login}&password={Password}";
+            byte[] Data = Encoding.ASCII.GetBytes(postData);
+            request.ContentLength = Data.Length;
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(Data, 0, Data.Length);
+            }
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Debug.WriteLine($"Статус выполннения: {response.StatusCode}");
+            string responseFromServer = "Печенька: токен = " + response.Cookies[0].Value.ToString();
+            Console.WriteLine(responseFromServer);
+        }
+
     }
 }
